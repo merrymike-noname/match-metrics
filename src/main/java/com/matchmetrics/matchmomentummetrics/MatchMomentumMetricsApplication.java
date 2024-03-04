1f4fa918bb0f71e1b481db2484cfe2643856c7a9
Mykhailo Kovalenko <kovalenko.mykhailo1@lll.kpi.ua>
2024-03-04 21:31:51 +0200

    Project init

---
+++ src/main/java/com/matchmetrics/matchmomentummetrics/MatchMomentumMetricsApplication.java
@@ -0,0 +1,13 @@
+package com.matchmetrics.matchmomentummetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchMomentumMetricsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatchMomentumMetricsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/matchmetrics/matchmomentummetrics/MatchMomentumMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matchmetrics.matchmomentummetrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchMomentumMetricsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
