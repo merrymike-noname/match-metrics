11620e92b53a674e1e8f7f5527ecb637b777a55c
Mykhailo Kovalenko <kovalenko.mykhailo1@lll.kpi.ua>
2024-03-05 15:51:57 +0200

    Refactor EloCsvClient for improved readability and performance

--- src/main/java/com/matchmomentummetrics/client/EloCsvClient.java
+++ src/main/java/com/matchmomentummetrics/client/EloCsvClient.java
@@ -4,32 +4,45 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Component
 public class EloCsvClient {
+
+    private static final String API_URL = "http://api.clubelo.com/";
+    private static final int ELO_RATING_POSITION = 4;
+
     public float getTeamElo(String teamName) {
+        String content = getTeamEloData(teamName);
+        return getEloFromContent(content);
+    }
+
+    private String getTeamEloData(String teamName) {
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> response = restTemplate
-                .getForEntity("http://api.clubelo.com/" + teamName, String.class);
-        String content = response.getBody();
+                .getForEntity(API_URL + teamName, String.class);
+        return response.getBody();
+    }
 
-        String lastLine = null;
-        String currentLine = null;
+    private float getEloFromContent(String content) {
+        String lastLine = getLastNonEmptyLine(content);
+        assert lastLine != null;
+        System.out.println(lastLine);
+        String[] currentTeamStats = lastLine.split(",");
+        return Float.parseFloat(currentTeamStats[ELO_RATING_POSITION]);
+    }
+
+    private String getLastNonEmptyLine(String content) {
         assert content != null;
-        int lastLineIndex = content.length();
-        while (lastLineIndex != -1) {
-            lastLineIndex = content.lastIndexOf("\n" , lastLineIndex - 1);
-            if (lastLineIndex > -1) {
-                currentLine = content.substring(lastLineIndex + 1);
-            }
-            if (currentLine!= null && !currentLine.isBlank()) {
-                lastLine = currentLine.trim();
-                break;
+        List<String> lines = Arrays.asList(content.split("\n"));
+        for (int i = lines.size() - 1; i >= 0; i--) {
+            String line = lines.get(i);
+            if (!line.isBlank()) {
+                return line;
             }
         }
-        assert lastLine != null;
-        String[] currentTeamStats = lastLine.split(",");
-
-        return Float.parseFloat(currentTeamStats[4]);
+        return null;
     }
 
 }
