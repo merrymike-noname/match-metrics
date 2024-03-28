3febfc9d0df069ca997079709785669103c81eb1
Mykhailo Kovalenko <kovalenko.mykhailo1@lll.kpi.ua>
2024-03-28 13:56:32 +0200

    Add logger configuration and usage example in MatchController Update .gitignore to ignore logs

--- src/main/java/com/matchmetrics/controller/MatchController.java
+++ src/main/java/com/matchmetrics/controller/MatchController.java
@@ -2,6 +2,8 @@ package com.matchmetrics.controller;
 
 import com.matchmetrics.entity.dto.match.MatchMainDto;
 import com.matchmetrics.service.MatchService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,7 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("matchmetrics/api/v0/matches")
 public class MatchController {
-
+    //private final Logger logger = LoggerFactory.getLogger(MatchController.class);
     private final MatchService matchService;
 
     @Autowired
@@ -20,6 +22,7 @@ public class MatchController {
 
     @GetMapping("/all")
     public List<MatchMainDto> getAllMatches() {
+        //logger.info("Returning all matches...");
         return matchService.getAllMatches();
     }
 
