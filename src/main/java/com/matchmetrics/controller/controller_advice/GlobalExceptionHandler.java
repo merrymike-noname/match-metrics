b028cf78e46318461f9c5a21a27c4fcfa4edb16b
kostiantyn <sinkkostia@gmail.com>
2024-04-02 22:34:14 +0300

    Finished ControllerAdvice. Added more test for MatchController

--- src/main/java/com/matchmetrics/controller/controller_advice/GlobalExceptionHandler.java
+++ src/main/java/com/matchmetrics/controller/controller_advice/GlobalExceptionHandler.java
@@ -1,7 +1,7 @@
 package com.matchmetrics.controller.controller_advice;
 
 import com.matchmetrics.entity.ApiErrorResponse;
-import com.matchmetrics.exception.FieldDoesNotExistException;
+import com.matchmetrics.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -21,8 +21,61 @@ public class GlobalExceptionHandler {
                 HttpStatus.BAD_REQUEST.value(),
                 HttpStatus.BAD_REQUEST.getReasonPhrase(),
                 e.getMessage(),
-                ((ServletWebRequest)request).getRequest().getRequestURI()
+                ((ServletWebRequest) request).getRequest().getRequestURI()
         );
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(DateConversionException.class)
+    public ResponseEntity<ApiErrorResponse> handleDateConversionException
+            (DateConversionException e, WebRequest request) {
+        ApiErrorResponse errorResponse = new ApiErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                e.getMessage(),
+                ((ServletWebRequest) request).getRequest().getRequestURI()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvalidDataException.class)
+    public ResponseEntity<ApiErrorResponse> handleInvalidDataException
+            (InvalidDataException e, WebRequest request) {
+        ApiErrorResponse errorResponse = new ApiErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                e.getMessage(),
+                ((ServletWebRequest) request).getRequest().getRequestURI()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MatchDoesNotExistException.class)
+    public ResponseEntity<ApiErrorResponse> handleMatchDoesNotExistException
+            (MatchDoesNotExistException e, WebRequest request) {
+        ApiErrorResponse errorResponse = new ApiErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                e.getMessage(),
+                ((ServletWebRequest) request).getRequest().getRequestURI()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(TeamDoesNotExistException.class)
+    public ResponseEntity<ApiErrorResponse> handleTeamDoesNotExistException
+            (TeamDoesNotExistException e, WebRequest request) {
+        ApiErrorResponse errorResponse = new ApiErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                e.getMessage(),
+                ((ServletWebRequest) request).getRequest().getRequestURI()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
 }
+
--- src/test/java/com/matchmetrics/MatchControllerTest.java
+++ src/test/java/com/matchmetrics/MatchControllerTest.java
@@ -1,9 +1,14 @@
 package com.matchmetrics;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.matchmetrics.controller.MatchController;
+import com.matchmetrics.controller.controller_advice.GlobalExceptionHandler;
+import com.matchmetrics.entity.dto.match.MatchAddUpdateDto;
 import com.matchmetrics.entity.dto.match.MatchMainDto;
 import com.matchmetrics.entity.dto.probability.ProbabilityMainDto;
+import com.matchmetrics.entity.dto.team.TeamNameDto;
 import com.matchmetrics.entity.dto.team.TeamNestedDto;
+import com.matchmetrics.exception.MatchDoesNotExistException;
 import com.matchmetrics.service.MatchService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,16 +19,24 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.validation.BindingResult;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
 
 @ExtendWith(MockitoExtension.class)
 public class MatchControllerTest {
@@ -36,10 +49,14 @@ public class MatchControllerTest {
 
     private MockMvc mockMvc;
 
+
     @BeforeEach
     public void setup() {
-        mockMvc = MockMvcBuilders.standaloneSetup(matchController).build();
+        mockMvc = MockMvcBuilders.standaloneSetup(matchController)
+                .setControllerAdvice(new GlobalExceptionHandler())
+                .build();
     }
+
 
     @Test
     public void testGetAllMatches() throws Exception {
@@ -111,4 +128,169 @@ public class MatchControllerTest {
         verify(matchService, times(1)).deleteMatch(id);
     }
 
+    @Test
+    public void testGetMatchesByCriteria_date_league() throws Exception {
+        String date1 = "2022-01-01";
+        String date2 = "2022-01-02";
+        int page = 1;
+        int perPage = 3;
+        String sortBy = "default";
+        String league = "LeagueBB";
+
+        TeamNestedDto homeTeam = new TeamNestedDto("Test1", "LeagueA", 1500);
+        TeamNestedDto awayTeam = new TeamNestedDto("Test2", "LeagueB", 1400);
+        TeamNestedDto newTeam = new TeamNestedDto("Test3", "LeagueC", 1300);
+        ProbabilityMainDto probability = new ProbabilityMainDto(0.4f, 0.3f, 0.3f);
+        ProbabilityMainDto probabilitySecond = new ProbabilityMainDto(0.5f, 0.25f, 0.025f);
+        MatchMainDto match1 = new MatchMainDto(new SimpleDateFormat("yyyy-MM-dd").parse(date1), "LeagueA", homeTeam, awayTeam, probability);
+        MatchMainDto match2 = new MatchMainDto(new SimpleDateFormat("yyyy-MM-dd").parse(date2), "LeagueBB", homeTeam, newTeam, probabilitySecond);
+
+        when(matchService.getMatchesByCriteria(any(), any(), eq(date1), any(), anyInt(), anyInt(), anyString())).thenReturn(List.of(match1));
+        when(matchService.getMatchesByCriteria(any(), any(), any(), eq(league), anyInt(), anyInt(), anyString())).thenReturn(List.of(match2));
+
+        //todo add date criteria to the test
+
+        mockMvc.perform(get("/matchmetrics/api/v0/matches")
+                        .param("date", date1)
+                        .param("page", Integer.toString(page))
+                        .param("perPage", Integer.toString(perPage))
+                        .param("sortBy", sortBy)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[{" +
+                        "\"date\":" + match1.getDate().getTime() + "," +
+                        "\"league\":\"" + match1.getLeague() + "\"," +
+                        "\"homeTeam\":{" +
+                        "\"name\":\"" + match1.getHomeTeam().getName() + "\"," +
+                        "\"country\":\"" + match1.getHomeTeam().getCountry() + "\"," +
+                        "\"elo\":" + match1.getHomeTeam().getElo() +
+                        "}," +
+                        "\"awayTeam\":{" +
+                        "\"name\":\"" + match1.getAwayTeam().getName() + "\"," +
+                        "\"country\":\"" + match1.getAwayTeam().getCountry() + "\"," +
+                        "\"elo\":" + match1.getAwayTeam().getElo() +
+                        "}," +
+                        "\"probability\":{" +
+                        "\"homeTeamWin\":" + match1.getProbability().getHomeTeamWin() + "," +
+                        "\"draw\":" + match1.getProbability().getDraw() + "," +
+                        "\"awayTeamWin\":" + match1.getProbability().getAwayTeamWin() +
+                        "}" +
+                        "}]"));
+
+        mockMvc.perform(get("/matchmetrics/api/v0/matches")
+                        .param("league", league)
+                        .param("page", Integer.toString(page))
+                        .param("perPage", Integer.toString(perPage))
+                        .param("sortBy", sortBy)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[{" +
+                        "\"date\":" + match2.getDate().getTime() + "," +
+                        "\"league\":\"" + match2.getLeague() + "\"," +
+                        "\"homeTeam\":{" +
+                        "\"name\":\"" + match2.getHomeTeam().getName() + "\"," +
+                        "\"country\":\"" + match2.getHomeTeam().getCountry() + "\"," +
+                        "\"elo\":" + match2.getHomeTeam().getElo() +
+                        "}," +
+                        "\"awayTeam\":{" +
+                        "\"name\":\"" + match2.getAwayTeam().getName() + "\"," +
+                        "\"country\":\"" + match2.getAwayTeam().getCountry() + "\"," +
+                        "\"elo\":" + match2.getAwayTeam().getElo() +
+                        "}," +
+                        "\"probability\":{" +
+                        "\"homeTeamWin\":" + match2.getProbability().getHomeTeamWin() + "," +
+                        "\"draw\":" + match2.getProbability().getDraw() + "," +
+                        "\"awayTeamWin\":" + match2.getProbability().getAwayTeamWin() +
+                        "}" +
+                        "}]"));
+    }
+
+    @Test
+    public void testGetMatchesByCriteria_date() throws Exception {
+        String date = "2022-01-01";
+        int page = 1;
+        int perPage = 3;
+        String sortBy = "default";
+
+        when(matchService.getMatchesByCriteria(any(), any(), eq(date), any(), anyInt(), anyInt(), anyString()))
+                .thenReturn(List.of(new MatchMainDto()));
+
+        mockMvc.perform(get("/matchmetrics/api/v0/matches")
+                        .param("date", date)
+                        .param("page", Integer.toString(page))
+                        .param("perPage", Integer.toString(perPage))
+                        .param("sortBy", sortBy)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testGetMatchesByCriteria_teamAndLeague() throws Exception {
+        String team = "Test1";
+        String league = "LeagueA";
+        int page = 1;
+        int perPage = 3;
+        String sortBy = "default";
+
+        when(matchService.getMatchesByCriteria(eq(team), any(), any(), eq(league), anyInt(), anyInt(), anyString()))
+                .thenReturn(List.of(new MatchMainDto()));
+
+        mockMvc.perform(get("/matchmetrics/api/v0/matches")
+                        .param("team", team)
+                        .param("league", league)
+                        .param("page", Integer.toString(page))
+                        .param("perPage", Integer.toString(perPage))
+                        .param("sortBy", sortBy)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testUpdateMatch() throws Exception {
+        int id = 1;
+        MatchAddUpdateDto matchDto = new MatchAddUpdateDto();
+        matchDto.setDate(new Date());
+        matchDto.setLeague("LeagueA");
+        matchDto.setHomeTeam(new TeamNameDto("Test1"));
+        matchDto.setAwayTeam(new TeamNameDto("Test2"));
+        matchDto.setProbability(new ProbabilityMainDto(0.4f, 0.3f, 0.3f));
+
+        MatchMainDto updatedMatch = new MatchMainDto();
+        updatedMatch.setDate(matchDto.getDate());
+        updatedMatch.setLeague(matchDto.getLeague());
+        updatedMatch.setHomeTeam(new TeamNestedDto(matchDto.getHomeTeam().getName(), "Country", 1500));
+        updatedMatch.setAwayTeam(new TeamNestedDto(matchDto.getAwayTeam().getName(), "Country", 1500));
+        updatedMatch.setProbability(matchDto.getProbability());
+
+        when(matchService.updateMatch(eq(id), any(MatchAddUpdateDto.class), any(BindingResult.class))).thenReturn(updatedMatch);
+
+        mockMvc.perform(put("/matchmetrics/api/v0/matches/update/{id}", id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(matchDto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.date").value(updatedMatch.getDate().getTime()))
+                .andExpect(jsonPath("$.league").value(updatedMatch.getLeague()))
+                .andExpect(jsonPath("$.homeTeam.name").value(updatedMatch.getHomeTeam().getName()))
+                .andExpect(jsonPath("$.awayTeam.name").value(updatedMatch.getAwayTeam().getName()))
+                .andExpect(jsonPath("$.probability.homeTeamWin").value(updatedMatch.getProbability().getHomeTeamWin()))
+                .andExpect(jsonPath("$.probability.draw").value(updatedMatch.getProbability().getDraw()))
+                .andExpect(jsonPath("$.probability.awayTeamWin").value(updatedMatch.getProbability().getAwayTeamWin()));
+    }
+
+
+    @Test
+    public void whenMatchDoesNotExistException_thenReturnsNotFound() throws Exception {
+        int nonExistingId = 111;
+
+        when(matchService.getMatchById(nonExistingId)).thenThrow(new MatchDoesNotExistException(nonExistingId));
+
+        mockMvc.perform(get("/matchmetrics/api/v0/matches/{id}", nonExistingId)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.status").value(404))
+                .andExpect(jsonPath("$.error").value("Not Found"))
+                .andExpect(jsonPath("$.message").value("Match with id " + nonExistingId + " does not exist."));
+    }
+
 }
+
