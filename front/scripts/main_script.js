ae467ce48b8ce90737523b557a47ac8ff119bc85
IvannaProgger <andrewkokakolni@gmail.com>
2024-05-22 17:18:31 +0300

    First page is ready

--- front/scripts/main_script.js
+++ front/scripts/main_script.js
@@ -21,6 +21,63 @@ document.addEventListener('DOMContentLoaded', function () {
         })
         .catch(error => console.error('Error:', error));
 
+    fetch(`http://localhost:8080/matchmetrics/api/v0/matches?homeTeam=Girona`)
+        .then(response => response.json())
+        .then(matches => {
+            if (matches.length > 0) {
+                displayMatch(matches[0]);
+            } else {
+                // If no home match is found, try to find an away match
+                return fetch(`http://localhost:8080/matchmetrics/api/v0/matches?awayTeam=Girona`)
+                    .then(response => response.json())
+                    .then(matches => {
+                        if (matches.length > 0) {
+                            displayMatch(matches[0]);
+                        } else {
+                            // If no match is found, display a message
+                            const favoriteMatchDiv = document.getElementById('favoriteTeamNextMatch');
+                            favoriteMatchDiv.textContent = 'No matches in the nearest time.';
+                        }
+                    });
+            }
+        })
+        .catch(error => console.error('Error:', error));
+
+    function displayMatch(match) {
+        const favoriteMatchDiv = document.getElementById('favoriteTeamNextMatch');
+        favoriteMatchDiv.padding = '40px';
+        const teamLogosDiv = favoriteMatchDiv.querySelector('.team-logos');
+        const homeTeamLogo = document.createElement('img');
+        homeTeamLogo.src = `/front/resources/logos/${match.homeTeam.name.toLowerCase()}.png`;
+        const awayTeamLogo = document.createElement('img');
+        awayTeamLogo.src = `/front/resources/logos/${match.awayTeam.name.toLowerCase()}.png`;
+        homeTeamLogo.onerror = function () {
+            this.onerror = null;
+            this.src = '/front/resources/countries/default.png';
+        };
+        awayTeamLogo.onerror = function () {
+            this.onerror = null;
+            this.src = '/front/resources/countries/default.png';
+        };
+        homeTeamLogo.style.width = '30px';
+        homeTeamLogo.style.height = '30px';
+        awayTeamLogo.style.width = '30px';
+        awayTeamLogo.style.height = '30px';
+        homeTeamLogo.style.marginRight = '20px !important';
+        awayTeamLogo.style.marginLeft = '20px !important';
+        teamLogosDiv.appendChild(homeTeamLogo);
+        teamLogosDiv.appendChild(awayTeamLogo);
+
+        const teamNamesDiv = favoriteMatchDiv.querySelector('.team-names');
+        teamNamesDiv.textContent = `${match.homeTeam.name}  vs  ${match.awayTeam.name}`;
+        teamNamesDiv.style.display = 'flex';
+        teamNamesDiv.style.alignItems = 'center';
+        teamNamesDiv.style.justifyContent = 'center';
+
+        const matchDateDiv = favoriteMatchDiv.querySelector('.match-date');
+        matchDateDiv.textContent = new Date(match.date).toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+    }
+
     forecastButton.addEventListener('click', function () {
         const team1 = team1Input.value.trim();
         const team2 = team2Input.value.trim();
@@ -47,7 +104,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 })
                 .catch(error => console.error('Error:', error));
 
-            predictionButton.style.display = 'block'; // Show the prediction button
+            predictionButton.style.display = 'block';
         } else {
             alert("Enter two real teams.");
         }
@@ -205,7 +262,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     matchInfoDiv.appendChild(probabilityDiv);
                     matchInfoDiv.style.backgroundColor = 'white';
                     matchInfoDiv.style.borderRadius = '10px';
-                    matchInfoDiv.style.padding = '5px';
+                    matchInfoDiv.style.padding = '20px';
 
                 } else {
                     const noMatchFoundMessage = document.createElement('p');
